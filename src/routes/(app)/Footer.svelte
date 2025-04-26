a6a072c5ffdb039aba8e8fbe85e7cc64dd721617
FlamingHerb <mbomisol@up.edu.ph>
2025-04-26 13:20:16 +0800

    chore: update footer

--- src/routes/(app)/Footer.svelte
+++ src/routes/(app)/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
     <!-- TODO: Add in buttons here. -->
-    <p>&copy; 2024 <a href="/">Michael Sean Brian B. Omisol</a>.</p>
+    <p>&copy; 2025 <a href="/">Michael Sean Brian B. Omisol</a>.</p>
 </footer>
 
 <style lang="scss">
